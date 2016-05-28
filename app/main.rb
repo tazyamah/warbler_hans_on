09d161523291330ec9a82e224db172ad65784b2e
Tatsuya Suzuki <tazyamah@gmail.com>
2016-05-29 08:17:45 +0900

    reading excelfile test

--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,20 @@
+require 'spreadsheet'
+
 def main
-  puts "hello warbler!"
+  Dir.glob('./sample_data/**/*.xls').each do |xlsfile|
+    xls = Spreadsheet.open(xlsfile)
+    puts xlsfile
+    xls.worksheets.each do |ws|
+      puts ws.name
+      (0..256).each do |x|
+        break unless ws[x,0]
+        (0..256).each do |y|
+          break unless ws[x,y]
+          print "[#{ws[x,y]}] "
+        end
+        print "\n"
+      end
+      puts "----------"
+    end
+  end
 end
