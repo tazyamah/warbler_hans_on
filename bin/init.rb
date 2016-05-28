72c6f55e2b3e37225142ad27b12f0e1cb14c4146
Tatsuya Suzuki <tazyamah@gmail.com>
2016-05-29 08:00:51 +0900

    add init.rb

---
+++ bin/init.rb
@@ -0,0 +1,10 @@
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/../"))
+ 
+Dir.glob(File.expand_path('vendor/bundle/jruby/*/gems/*/lib')).each do |path|
+  $LOAD_PATH << path if !$LOAD_PATH.include?(path)
+end
+ 
+require 'app/main'
+ 
+main
+
