da38e93cda07082db288210c8557114195e4546b
Tatsuya Suzuki <tazyamah@gmail.com>
2016-05-30 17:44:48 +0900

    output to txtfile

--- app/main.rb
+++ app/main.rb
@@ -1,22 +1,25 @@
 require 'spreadsheet'
 
 def main
+  result = ""
   Dir.glob('./sample_data/**/*.xls').each do |xlsfile|
     xls = Spreadsheet.open(xlsfile)
-    puts xlsfile
+    result << "#{xlsfile}\n"
     xls.worksheets.each do |ws|
-      puts ws.name
+      result << "#{ws.name}\n"
       (0..256).each do |x|
         break unless ws[x,0]
         (0..256).each do |y|
           break unless ws[x,y]
-          print "[#{ws[x,y]}] "
+          result << "[#{ws[x,y]}] "
         end
-        print "\n"
+        result << "\n"
       end
-      puts "----------"
+      result << "----------\n"
     end
   end
+  puts result
+  File.open("./result.txt","w"){|io| io.write(result)}
 end
 
 def dummy
