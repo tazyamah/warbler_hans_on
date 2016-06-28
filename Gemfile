b90a478ae13309f468a46935a4012a1e3d221358
Tatsuya Suzuki <tazyamah@gmail.com>
2016-06-28 15:04:38 +0900

    add sample

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source "https://rubygems.org"
 gem 'warbler'
 gem 'spreadsheet'
 gem 'mail'
+#gem 'rubyzip'
+gem 'zip-zip'
 gem 'rspec'
--- app/main.rb
+++ app/main.rb
@@ -1,4 +1,6 @@
 require 'spreadsheet'
+require 'zip'
+require 'mail'
 
 require 'lib/sample.jar'
 java_import org.tazyamah.sample.SampleClass
@@ -27,6 +29,44 @@ def main
   end
   puts result
   File.open("./result.txt","w"){|io| io.write(result)}
+
+  # zip生成
+  zip = Zip::OutputStream.write_buffer(
+      ::StringIO.new(''),
+      Zip::TraditionalEncrypter.new('test1234')
+    ) do |buf|
+      buf.put_next_entry('./sample_data/book.xls')
+#      buf.put_next_entry('./sample_data/testcontent.txt')
+      #buf.write(open('./sample_data/book.xls'){|f|f.read})
+      #buf.put_next_entry('./sample_data/testcontent.txt')
+    end.string
+#  zip = Zip::Archive.open_buffer(Zip::CREATE)
+#  zip.add_buffer('./sample_data/book.xls', 'contents')
+#  zip.add_buffer('./sample_data/testcontent.txt', 'contents')
+#  zip.encrypt('test1234')
+#  zzz =  zip.read
+#  zip.close
+
+  # emlファイル生成テスト
+  body = <<-EOB
+    これはテストメール本文です。
+    改行や
+
+    空行が
+    あっても問題なく出力されるはずです。
+  EOB
+  mail = Mail.new do
+    from      'from@example.com'
+    to        'to@example.com'
+    subject   'これはテストメールです'
+    body      body
+    #add_file  filename: '添付ファイル.zip', content: zip.string
+    add_file  filename: '添付ファイル.zip', content: zip
+  end
+
+  #File.open("result.zip", "wb:UTF-8:UTF-8"){|io| io.write(zip.string)}
+  File.open("result.zip", "wb"){|io| io.write(zip)}
+  File.open("result.eml", "wb"){|io| io.write(mail.encoded)}
 end
 
 def dummy
